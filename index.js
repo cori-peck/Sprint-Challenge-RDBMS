9becb0fe9a0575386a9671d739576717371effdc
cori-peck <cori.peck@ymail.com>
2019-03-29 11:54:02 -0500

    Create projects looks like it is working but is only returning the id in the body, need to create a get to check...

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ server.use(express.json());
 server.use(helmet());
 
 
+server.get('/', (req, res) => {
+    res.status(200).send("Project Tracker - David Allen's Getting Things Done Methodology")
+})
+
+
+// C - Create 
+// /api/projects
+server.post('/api/projects', (req, res) => {
+    db('projects').insert(req.body)
+    .then(projectId => {
+        const [ id ] = projectId;
+        res.status(201).json({ id: id});
+    })
+    .catch(err => {
+        res.status(500).json({ message: "Could not add the project" })
+    })
+})
+
+
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
     console.log(`*** Server is up and running on localhost:${port} ***`)
